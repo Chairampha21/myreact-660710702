a1903f9eea0672061d5349752e331c0754649d6e
Chairampha21 <Khrueapan_c@su.ac.th>
2025-08-07 05:10:05 +0000

    JSXExample

---
+++ chairampha-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.png';
+import './App.css';
+import Hello from './Hello';  // import Hello component
+import JSXExamples from './Components/JSXExamples'; 
+
+function App() { 
+  return (
+    <div className="App">
+      <Hello/>
+      <JSXExamples/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello This is my React!
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
